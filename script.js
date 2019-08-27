f9b659dd66979858c690ab66d2b853f0087bf9be
koichi-inoue <inoue.ko@gmail.com>
2019-08-27 19:51:17 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+// GlobalValue
+
+var width = 480;
+var height = 360;
+
+var webcam;
+var DC;
+
+var localMediaStream;
+var myTimer;
+
+
+// Functions
+
+window.onload = function(){
+
+  document.querySelector("#start").addEventListener("click", Start, false);
+  document.querySelector("#stop").addEventListener("click", Stop, false);
+  document.querySelector("#mirror").addEventListener("click", Mirror, false);
+
+  navigator.getUserMedia = navigator.getUserMedia
+                      || navigator.webkitGetUserMedia
+                      || navigator.mozGetUserMedia;
+
+  window.URL = window.URL || window.webkitURL;
+
+  webcam = document.getElementById("webcam");
+  DC = document.getElementById("cnvs").getContext('2d');
+
+  navigator.getUserMedia( { video: true, audio: false },success, error );
+  start();
+}
+
+function success(stream) { // for success case
+  webcam.srcObject = stream;
+  console.log(stream);
+}
+
+function error(err) { // for error case
+  console.log("getUserMedia error: ", err);
+}
+
+// Button Start
+function Start(){
+  myTimer = setInterval(Draw,1000/30);
+}
+
+  function Draw(){ // Drawing loop
+    DC.drawImage(webcam, 0, 0, width, height);
+  }
+
+// Button Stop
+function Stop(){
+  clearInterval(myTimer);
+}
+
+// Button Mirror
+function Mirror(){
+  DC.translate(width,0);
+  DC.scale(-1,1);
+}
